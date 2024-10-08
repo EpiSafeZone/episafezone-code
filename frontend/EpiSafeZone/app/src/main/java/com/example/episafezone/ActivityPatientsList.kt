2b630433047be9bbf12cb2762f83c67204b34b21
el-kiosquet <obustosgimeno@gmail.com>
2024-10-08 13:53:26 +0200

    Changed named of main class and solved naming errors
    From MainActivity to ActivityPatientsList

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/ActivityPatientsList.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/ActivityPatientsList.kt
@@ -5,16 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.episafezone.databinding.ActivityMainBinding
+import com.example.episafezone.databinding.ActivityPatientsListBinding
 
-class PatientsList : AppCompatActivity() {
+class ActivityPatientsList : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityPatientsListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater);
+        binding = ActivityPatientsListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->

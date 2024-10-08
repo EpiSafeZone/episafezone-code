842f6db6feb1b2864532a84ae4f4dc7d5aa257fd
Mejias32r <mejiasriobopepe@gmail.com>
2024-10-08 10:01:59 +0200

    creación proyecto backend

---
+++ backend/EpiSafeZone/src/main/java/com/example/episafezone/EpiSafeZoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.episafezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiSafeZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpiSafeZoneApplication.class, args);
+    }
+
+}
---
+++ backend/EpiSafeZone/src/test/java/com/example/episafezone/EpiSafeZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.episafezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiSafeZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

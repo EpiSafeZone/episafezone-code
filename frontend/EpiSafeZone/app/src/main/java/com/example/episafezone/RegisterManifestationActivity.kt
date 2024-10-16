f342b8615dd406ab4ec0f3c3dbdd7313666e67ba
cgimcas <cesargicas@gmail.com>
2024-10-16 11:40:56 +0200

    Fix: Corregido error de compilación.

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/RegisterManifestationActivity.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/RegisterManifestationActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.episafezone.databinding.ActivityMainBinding
 import com.example.episafezone.databinding.ActivityRegisterManifestationBinding
 
 class RegisterManifestationActivity : AppCompatActivity() {

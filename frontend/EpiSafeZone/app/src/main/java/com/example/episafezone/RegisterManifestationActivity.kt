6d033a3506335f7cb53d1f93086bbb3cc15d67ce
cgimcas <cesargicas@gmail.com>
2024-10-16 09:33:36 +0200

    WIP: Crear la actividad.

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/RegisterManifestationActivity.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/RegisterManifestationActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.episafezone.databinding.ActivityMainBinding
+import com.example.episafezone.databinding.ActivityRegisterManifestationBinding
 
 class RegisterManifestationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterManifestationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityRegisterManifestationBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register_manifestation)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
     }
 }

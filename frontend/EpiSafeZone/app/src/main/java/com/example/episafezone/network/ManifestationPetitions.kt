85d86c03e0ea6bf46bcdc832c0cd3ef1ca83393c
cgimcas <cesargicas@gmail.com>
2024-10-20 12:16:32 +0200

    WIP: Creacion de las peticiones.

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/network/ManifestationPetitions.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/network/ManifestationPetitions.kt
@@ -2,10 +2,24 @@ package com.example.episafezone.network
 
 import android.content.Context
 import android.widget.Toast
+import com.example.episafezone.BuildConfig
+import com.example.episafezone.ProfileActivity
 import com.example.episafezone.models.Manifestation
 
 object ManifestationPetitions {
-    fun getManifestations(context: Context) :  List<Manifestation> {
+
+    private const val myIP = BuildConfig.API_LINK
+    private val url = "http://$myIP"
+
+    lateinit var orderVolleyQueue: RequestQueue
+
+    fun initializeQueue(){
+        //TODO: Call this method in the profile activity
+        orderVolleyQueue = Volley.newRequestQueue(ProfileActivity.getContext())
+    }
+
+    //TODO: Add as a parameter of this method a reference to the patient
+    fun getManifestations() :  List<Manifestation> {
         //TODO: Implement this method
         return emptyList()
     }

524dcdf8410cc462520b79e915e10ccd6a157d36
Antopato <90763997+Antopato@users.noreply.github.com>
2024-10-21 19:41:13 +0200

    Medicines and user info added to view

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/models/Patient.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/models/Patient.kt
@@ -6,9 +6,9 @@ class Patient(
     var id : Int,
     var name : String,
     var surname : String,
-    var heigth : String,
+    var height : String,
     var weight: Int,
-    val birthdate: Date ,
-    val age: Int ,
-    val color: String,
+    val birthdate: Date?,
+    val age: Int?,
+    val color: String?,
 )

5062c3f3501c5e7a0885ab925a67b260730347b2
el-kiosquet <obustosgimeno@gmail.com>
2024-10-21 12:36:21 +0200

    Create PatientListAdapter.kt
    Class still not implemented

---
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/adapter/PatientListAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.episafezone.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PatientListAdapter : RecyclerView.Adapter<PatientListAdapter.ViewHolder>() {
+    class ViewHolder(view:View) : RecyclerView.ViewHolder(view) {
+        //val patientName = view.findViewById()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+
+    }
+}

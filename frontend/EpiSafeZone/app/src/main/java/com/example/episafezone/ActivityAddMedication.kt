5a9b0ee30a6b289576ea2d383d3a781135772664
Antopato <90763997+Antopato@users.noreply.github.com>
2024-10-22 20:16:48 +0200

    Modifications in navegavility

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/ActivityAddMedication.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/ActivityAddMedication.kt
@@ -1,11 +1,15 @@
 package com.example.episafezone
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.episafezone.businesslogic.MedicationLogic
 import com.example.episafezone.databinding.ActivityAddMedicationBinding
+import com.example.episafezone.databinding.ActivityProfileBinding
 import com.example.episafezone.models.Medication
+import com.example.episafezone.models.Patient
 import com.example.episafezone.models.Reminder
+import com.example.episafezone.network.PatientsListPetitions
 
 class ActivityAddMedication : AppCompatActivity() {
 
@@ -16,6 +20,8 @@ class ActivityAddMedication : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddMedicationBinding.inflate(layoutInflater);
         setContentView(binding.root)
+        val patient = intent.getSerializableExtra("patient") as Patient
+        contextObj = this
 
         binding.alarmCheckBox.setOnCheckedChangeListener{_,isChecked ->
             binding.alarmHoursInput.isEnabled=isChecked;
@@ -42,10 +48,18 @@ class ActivityAddMedication : AppCompatActivity() {
             }
 
             try{
-                medicationLogic.createMedication(name,dosis,unit,alarm,times,nextAlarm);
+                medicationLogic.createMedication(name,dosis,unit,alarm,times,nextAlarm,patient);
             }catch(e : Exception){
                 binding.errorText.text=e.message.toString();
             }
+        }
+    }
+    companion object{
+        private lateinit var contextObj: Context
+        private lateinit var binding: ActivityProfileBinding
+
+        fun getContext() : Context {
+            return contextObj
         }
     }
 }

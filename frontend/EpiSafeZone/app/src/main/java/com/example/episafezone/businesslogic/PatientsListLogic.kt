a6689f8458e5bbf9bed3abe591f87177a1b24701
Antopato <90763997+Antopato@users.noreply.github.com>
2024-10-23 12:05:48 +0200

    Minor problems

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/businesslogic/PatientsListLogic.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/businesslogic/PatientsListLogic.kt
@@ -1,11 +1,12 @@
 package com.example.episafezone.businesslogic
 
-import com.example.episafezone.ActivityPatientsList
 import com.example.episafezone.models.Patient
 import com.example.episafezone.network.PatientsListPetitions
 import java.util.Date
 
 object PatientsListLogic {
+
+
 
     fun getProfileInfo(patient : Patient) {
 

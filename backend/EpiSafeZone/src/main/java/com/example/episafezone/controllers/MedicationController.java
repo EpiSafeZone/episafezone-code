f5ad36a5ef90140bb7d18e3585aa6f7be54d8f2a
SergioMG2 <sergi03mart1@gmail.com>
2024-10-23 12:49:59 +0200

    Create and update medication
    Creación de los métodos create y update para medications

--- backend/EpiSafeZone/src/main/java/com/example/episafezone/controllers/MedicationController.java
+++ backend/EpiSafeZone/src/main/java/com/example/episafezone/controllers/MedicationController.java
@@ -7,10 +7,7 @@ import com.example.episafezone.services.MedicationService;
 import com.example.episafezone.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,12 +24,23 @@ public class MedicationController {
     }
 
     @GetMapping(path="/{id}")
-    public @ResponseBody Optional<Medication> getAll(@PathVariable int id){return medicationService.findById(id);
+    public @ResponseBody Medication getAll(@PathVariable int id){return medicationService.findById(id);
     }
 
     @GetMapping(path="/patient/{patientId}")
     public @ResponseBody List<Medication> getMedicationsByPatient(@PathVariable Integer patientId) {
         return medicationService.findMedicationsByPatient(patientId);
+    }
+
+    @PostMapping(path="/create")
+    public @ResponseBody Medication createMedication(@RequestBody Medication medication){
+        return medicationService.create(medication);
+    }
+
+    @PutMapping(path="/update/{id}")
+    public @ResponseBody Medication updateMedication(@PathVariable Integer id,
+                                                     @RequestBody Medication medication){
+        return medicationService.update(id, medication);
     }
 
 

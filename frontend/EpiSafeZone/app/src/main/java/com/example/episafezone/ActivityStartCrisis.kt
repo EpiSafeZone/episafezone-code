9a8781011db695d70c1469e95937733c17c7f3ea
Cesar Gimeno Castellote <cesargicas@gmail.com>
2024-10-30 18:21:57 +0100

    WIP: Crear iniciar crisis

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/ActivityStartCrisis.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/ActivityStartCrisis.kt
@@ -1,6 +1,9 @@
 package com.example.episafezone
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.text.InputType
 import android.view.Gravity
@@ -9,8 +12,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentHostCallback
 import com.example.episafezone.businesslogic.ProfileLogic
 import com.example.episafezone.businesslogic.StartCrisisLogic
+import com.example.episafezone.businesslogic.utils.TimerService
 import com.example.episafezone.databinding.ActivityStartCrisisBinding
 import com.example.episafezone.models.Patient
 import com.example.episafezone.network.StartCrisisPetitions
@@ -24,12 +29,22 @@ class ActivityStartCrisis : AppCompatActivity() {
 
     private val gson = Gson()
 
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(TimerService.TIMER_EXTRA, 0.0)
+            binding.chronoTextView.text = StartCrisisLogic.getTimeStringFromDouble(time)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityStartCrisisBinding.inflate(layoutInflater)
         setContentView(binding.root)
         contextObj = this
+
+        serviceIntent = Intent(this, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
 
         val json = intent.getSerializableExtra("patient") as String
         patient = gson.fromJson(json,Patient::class.java)
@@ -43,11 +58,12 @@ class ActivityStartCrisis : AppCompatActivity() {
         }
     }
 
-
-
     companion object{
         private lateinit var contextObj: Context
         private lateinit var binding: ActivityStartCrisisBinding
+        lateinit var serviceIntent: Intent
+
+        var time = 0.0
 
         fun getContext() : Context {
             return contextObj
--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/businesslogic/StartCrisisLogic.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/businesslogic/StartCrisisLogic.kt
@@ -1,9 +1,14 @@
 package com.example.episafezone.businesslogic
 
 import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
 import com.example.episafezone.ActivityPatientsList
 import com.example.episafezone.ActivityProfile
 import com.example.episafezone.ActivityStartCrisis
+import com.example.episafezone.R
+import com.example.episafezone.businesslogic.utils.TimerService
 import com.example.episafezone.databinding.ActivityStartCrisisBinding
 import com.example.episafezone.models.Patient
 import com.example.episafezone.network.StartCrisisPetitions
@@ -12,18 +17,14 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.util.Timer
 import java.util.TimerTask
+import kotlin.concurrent.timerTask
+import kotlin.math.roundToInt
 
 object StartCrisisLogic {
-
-    private lateinit var timerTask : TimerTask
 
     private val gson = Gson()
 
     private var timerStarted = false
-
-    private var timer = Timer()
-
-    private var time = 0.0
 
     fun getProfileLogic(patient: Patient) {
         StartCrisisPetitions.getProfileInfo(patient)
@@ -41,15 +42,34 @@ object StartCrisisLogic {
     }
 
     fun startStopTimer(binding: ActivityStartCrisisBinding) {
-        if (timerStarted) {
-            timerStarted = false
-            binding.button.text = "Detener"
-            //TODO: Continuar logica
-            // https://www.youtube.com/watch?v=7QVr5SgpVog
-        } else {
-            timerStarted = true
-            binding.button.text = "Empezar"
-            //TODO: Continuar logica
-        }
+        if(timerStarted)
+            stopTimer(binding)
+        else
+            startTimer(binding)
+    }
+
+    private fun startTimer(binding: ActivityStartCrisisBinding) {
+        ActivityStartCrisis.serviceIntent.putExtra(TimerService.TIMER_EXTRA, ActivityStartCrisis.time)
+        ActivityStartCrisis.getContext().startService(ActivityStartCrisis.serviceIntent)
+        binding.button.text = "Detener"
+        binding.button.setBackgroundColor(getColor(ActivityStartCrisis.getContext(), R.color.red))
+        timerStarted = true
+    }
+
+    private fun stopTimer(binding: ActivityStartCrisisBinding) {
+        // TODO: Implementar logica registrar manifestación
+        Toast.makeText(ActivityStartCrisis.getContext(), "Parar timer", Toast.LENGTH_SHORT).show()
+    }
+
+    fun getTimeStringFromDouble(time: Double): String {
+        val resultInt = time.roundToInt()
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(minutes, seconds)
+    }
+
+    fun makeTimeString(min: Int, sec: Int): String {
+        return String.format("%02d:%02d", min, sec)
     }
 }

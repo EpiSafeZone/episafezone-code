583609eca16c4183043b8c27a7631cfa828b6c40
Mejias32r <mejiasriobopepe@gmail.com>
2024-11-11 09:15:14 +0100

    Cambio en el manifestation Controller
    faltaba un / para poder usar bien la url

--- backend/EpiSafeZone/src/main/java/com/example/episafezone/controllers/ManifestationController.java
+++ backend/EpiSafeZone/src/main/java/com/example/episafezone/controllers/ManifestationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping(path = "manifestation")
+@RequestMapping(path = "/manifestation")
 public class ManifestationController {
     @Autowired
     ManifestationService manifestationService;

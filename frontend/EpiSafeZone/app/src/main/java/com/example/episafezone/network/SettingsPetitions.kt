fb2188b060037c052f6853b6172bbeecc3753b1f
Antopato <90763997+Antopato@users.noreply.github.com>
2025-01-22 19:27:03 +0100

    Añadido back para compartir perfil

--- frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/network/SettingsPetitions.kt
+++ frontend/EpiSafeZone/app/src/main/java/com/example/episafezone/network/SettingsPetitions.kt
@@ -3,10 +3,14 @@ package com.example.episafezone.network
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.episafezone.ActivitySettings
 import com.example.episafezone.BuildConfig
+import com.example.episafezone.models.Patient
+import com.example.episafezone.models.User
+import org.json.JSONObject
 
 object SettingsPetitions {
     private const val myIP = BuildConfig.API_LINK
@@ -29,5 +33,30 @@ object SettingsPetitions {
                 Log.d("GetNoDisturbHours",error.message.toString())
             })
         StartCrisisPetitions.orderVolleyQueue.add(stringRequest)
+    }
+
+    fun sharePatient(user : Int, email : String, patient: Patient, register : Boolean,
+                     profile : Boolean, medicine : Boolean, tutor: Boolean) {
+        val jsonObject = JSONObject()
+        jsonObject.put("tutorSharingId",user)
+        jsonObject.put("tutorReceivingEmail",email)
+        jsonObject.put("patientId",patient.id)
+        jsonObject.put("registerCrisisPermission",register)
+        jsonObject.put("profilePermission",profile)
+        jsonObject.put("medicinePermission",medicine)
+        jsonObject.put("tutorPermission",tutor)
+
+        println(jsonObject)
+
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.POST,
+            "$url/tutor/share",
+            jsonObject,
+            {response->
+            },
+            {error->
+            })
+
+        orderVolleyQueue.add(jsonRequest)
     }
 }

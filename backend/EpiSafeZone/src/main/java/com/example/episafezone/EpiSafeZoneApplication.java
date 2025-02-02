011ebf12195c54f0f10a21f46c20d2830f91236e
Cesar Gimeno Castellote <cesargicas@gmail.com>
2025-02-02 23:51:14 +0100

    Test: prueba para ejecutar la github action.

--- backend/EpiSafeZone/src/main/java/com/example/episafezone/EpiSafeZoneApplication.java
+++ backend/EpiSafeZone/src/main/java/com/example/episafezone/EpiSafeZoneApplication.java
@@ -3,7 +3,7 @@ package com.example.episafezone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+//Test to try github action.
 @SpringBootApplication
 public class EpiSafeZoneApplication {
 
